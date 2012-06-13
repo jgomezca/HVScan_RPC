9fab7b573ee0b1ab13e8459732514dffc153f152
Miguel Ojeda <mojedasa@cern.ch>
2012-06-13 18:23:36 +0200

    Case-insensitive sorting of the services' names.
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

--- keeper/keeper.py
+++ keeper/keeper.py
@@ -12,7 +12,7 @@ __email__ = 'mojedasa@cern.ch'
 
 import config
 
-services = sorted(list(config.servicesConfiguration))
+services = sorted(list(config.servicesConfiguration), key = str.lower)
 
 
 import os
