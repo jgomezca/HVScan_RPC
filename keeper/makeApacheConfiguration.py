2abde7f3a30452514cc815f9ac2325f60b379876
Miguel Ojeda <mojedasa@cern.ch>
2012-07-25 12:36:05 +0200

    Remove unused LogFormat directives.
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

--- keeper/makeApacheConfiguration.py
+++ keeper/makeApacheConfiguration.py
@@ -384,9 +384,6 @@ HostnameLookups Off
 ErrorLog logs/error_log
 LogLevel warn
 LogFormat "%h %l %u %t \\"%r\\" %>s %b \\"%{{Referer}}i\\" \\"%{{User-Agent}}i\\"" combined
-LogFormat "%h %l %u %t \\"%r\\" %>s %b" common
-LogFormat "%{{Referer}}i -> %U" referer
-LogFormat "%{{User-agent}}i" agent
 CustomLog logs/access_log combined
 
 ServerSignature Off
