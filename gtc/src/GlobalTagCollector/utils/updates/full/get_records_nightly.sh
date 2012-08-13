ea730360ccdae2de221fab4fd3187e61c3224efc
Kostas Tamosiunas <kostast@cern.ch>
2012-08-13 14:54:28 +0200

    Code cleanup
    * Removed no more needed manage.py command
    * Removed GlobalTagCollector/utils package with no more used code (data is taken from records provider)
    
    Signed-off-by: Kostas Tamosiunas <kostast@cern.ch>
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

--- gtc/src/GlobalTagCollector/utils/updates/full/get_records_nightly.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-hardware_arch=$1
-nightly_path=$2
-#slc5_amd64_gcc434
-base_path=/afs/cern.ch/cms/sw/ReleaseCandidates/$hardware_arch
-export CMSSW_RELEASE_BASE=$base_path
-for plug in ${CMSSW_RELEASE_BASE}/$nightly_path/pluginCondCore*Plugins.so
-    do nm -C $plug 2>&1 | sed "/nm: .* Plugins.so\': No such file/ d" | grep "vtable for DataProxy<" | sed "s/^.* DataProxy<//g" | sed "s/>$//g"
-done
