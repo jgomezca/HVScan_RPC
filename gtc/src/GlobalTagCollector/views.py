6fa43bf3e22854e12e27e75daa029b23bcf28430
Kostas Tamosiunas <kostast@cern.ch>
2012-08-20 10:29:18 +0200

    Fixed bug: Wrong column name was used for ordering software releases.
    Instead of comparing 2 software release versions, software release
    version and software release id were compared.
    
    Signed-off-by: Kostas Tamosiunas <kostast@cern.ch>
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

--- gtc/src/GlobalTagCollector/views.py
+++ gtc/src/GlobalTagCollector/views.py
@@ -83,7 +83,7 @@ def json_queues_for_record(request): #TODO try except
     lowest_internal_version = record_releases[0].internal_version
     higest_internal_version = record_releases[-1].internal_version
     #TODO hardware ach , open.
-    queue_list = GTQueue.objects.filter(release_from__lte=higest_internal_version).filter(Q(release_to__gte=lowest_internal_version) | Q(release_to=None)).order_by('name')
+    queue_list = GTQueue.objects.filter(release_from__internal_version__lte=higest_internal_version).filter(Q(release_to__internal_version__gte=lowest_internal_version) | Q(release_to=None)).order_by('name')
     obj_dics = [{'id': obj.id, 'name':obj.name, 'descr':obj.description } for obj in queue_list] #obj.description
     return HttpResponse(json.dumps(obj_dics), mimetype="application/json")
 
