a214ef825471b59c8c24abc5182745b64dde7dfb
Kostas Tamosiunas <kostast@cern.ch>
2012-10-04 15:09:03 +0200

    Removed no more used Shibboleth_CERN package
    Signed-off-by: Kostas Tamosiunas <kostast@cern.ch>
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

--- gtc/src/gtc/settings.py
+++ gtc/src/gtc/settings.py
@@ -171,7 +171,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'GlobalTagCollector',
-    'Shibboleth_CERN',
     'django.contrib.humanize',
     'django.contrib.sites',
     'django.contrib.flatpages',
