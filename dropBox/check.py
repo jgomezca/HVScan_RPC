44171c4efff0800a2d79151d0cd71dcee8ecc4ff
Miguel Ojeda <mojedasa@cern.ch>
2012-10-09 12:43:00 +0200

    Clean up, improve and document checkStructure().
      * Now matching uses tuple for matching several possibilities,
        dict for matching dictionaries with support for required keys,
        optional keys and arbitrary keys, and list for matching lists.
        This allows for easy flexible matching of JSON.
    
      * Added the missingKey test case.
    
      * Updated the other test cases for the new error messages.
    
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

--- dropBox/check.py
+++ dropBox/check.py
@@ -47,17 +47,21 @@ def checkStructure(metadata, structure):
     '''Checks the structure of the metadata.
     '''
 
-    #-mos XXX: I will clean this: Explain how it works, add proper error
-    #          messages with a proper chain from the top levels, etc...
-    #          Then move it to common because it can be useful for others
-    #          to some file like type.py or checkType.py.
-    #          Also, check that there are no more keys than those specified.
+    #-mos TODO: Improve error messages for nested dictionaries/structures.
+    #-mos TODO: Move to common, maybe, since this can be useful for others.
+    #-mos TODO: Since this was thought to match JSON, we can use tuple with
+    #           a special meaning and other types like set will not be present
+    #           in JSON either. However, if want to make this generic, add a
+    #           special class to match a possible set of matches (i.e. what
+    #           the current tuple does), and then add support for iterating
+    #           over sets/tuples/etc. in the "value" case (i.e. else) like
+    #           it is done now for lists.
 
     if type(structure) == tuple:
-        if metadata not in structure:
-            raise dropBox.DropBoxError('In the metadata, value %s is not any of %s.' % (str(metadata), str(structure)))
+        # If the type is a tuple, check whether the data matches any of the structures.
+        # This can be used to match against several types, values, etc.
+        # e.g. 'asdf', 123 and 1.3 matches (int, str, 1.3), but 1.2 does not.
 
-    elif type(structure) == set:
         ok = False
         for item in structure:
             try:
@@ -66,41 +70,79 @@ def checkStructure(metadata, structure):
                 break
             except dropBox.DropBoxError:
                 pass
+
         if not ok:
-            raise dropBox.DropBoxError('In the metadata, type %s is not any of %s.' % (type(metadata).__name__, [x.__name__ for x in structure]))
-        return
+            raise dropBox.DropBoxError('In the metadata, %s does not match the expected structure.' % metadata)
 
     elif type(structure) == dict:
-        for key in structure.keys():
-            if type(key) == unicode:
-                # Key with a fixed name, check value
-                (requiredKey, valueStructure) = structure[key]
+        # If the type is a dictionary, the structure can contain two kinds of
+        # keys: values or types. First, keys in data are matched with the values
+        # i.e. fixed keys. If found, their value is a tuple (isRequiredKey, value).
+        # Value is as usual the structure for the value in the data, and isRequiredKey
+        # allows to specify whether this key must be in the data.
+        # Then, if a key did not match any of the fixed keys, the key is matched
+        # against the types.
+        #
+        # Example:
+        #
+        # {
+        #     u'myRequiredKey': (True, str),
+        #     u'myOptionalKey': (False, int),
+        #     unicode: int,
+        # }
+        #
+        # This structure tells that the data dictionary must contain a required
+        # key with name myRequiredKey and that its value must be a string.
+        # Then, the dictionary may contain myOptionalKey, which in that case
+        # its value must be an integer.
+        # Finally, other keys of type unicode are allowed, which their value
+        # must be of type int.
 
-                try:
-                    value = metadata[key]
-                except KeyError:
-                    if not requiredKey:
-                        raise dropBox.DropBoxError('In the metadata, key %s is required.' % key)
-                    continue
+        fixedKeys = set([x for x in structure.keys() if type(x) != type])
+        typeKeys = set([x for x in structure.keys() if type(x) == type])
 
-                checkStructure(value, valueStructure)
-            else:
-                # Other kind
-                for metadataKey in metadata:
-                    checkStructure(metadataKey, key)
-                    checkStructure(metadata[metadataKey], structure[key])
+        requiredFixedKeys = set([x for x in fixedKeys if structure[x][0]])
+
+        for key in metadata:
+            # First try to match the key with the structure's fixed keys, if any.
+            if key in fixedKeys:
+                requiredFixedKeys.discard(key)
+                checkStructure(metadata[key], structure[key][1])
+                continue
+
+            # If the key did not match a fixed one, let's try to match it
+            # with one of the types, if any.
+            if type(key) in typeKeys:
+                checkStructure(metadata[key], structure[type(key)])
+                continue
+
+            raise dropBox.DropBoxError('In the metadata, key %s does not match the expected structure.' % key)
+
+        # Now check that all the required keys were found
+        if len(requiredFixedKeys) != 0:
+            raise dropBox.DropBoxError('In the metadata, keys %s are required and were not found.' % list(requiredFixedKeys))
 
     elif type(structure) == list:
+        # If the type is a list, we match all elements in the data against
+        # the element of the list.
+        # i.e. we only allow here to match an entire list with one structure,
+        # so it is not possible to match by position.
+        # e.g. [int] means a list of ints so [1,2,3] would match.
+        # e.g. [(int, float)] means a list of int or flots so [1,1.2] would match.
         for item in metadata:
             checkStructure(item, structure[0])
 
     elif type(structure) == type:
-        if type(metadata) != structure:
-            raise dropBox.DropBoxError('In the metadata, type %s is not equal to %s.' % (type(metadata).__name__, structure.__name__))
+        # If the type is a type itself, e.g. we match against unicode, we check
+        # whether the data is an instance of that type.
+        if not isinstance(metadata, structure):
+            raise dropBox.DropBoxError('In the metadata, type %s is not and instance of %s.' % (type(metadata).__name__, structure.__name__))
 
     else:
-        if type(metadata) != type(structure):
-            raise dropBox.DropBoxError('In the metadata, type %s is not equal to %s.' % (type(metadata).__name__, type(structure).__name__))
+        # If the type is not a tuple, dict or list and it is not a type itself,
+        # we consider that this is a value so we compare it directly.
+        if metadata != structure:
+            raise dropBox.DropBoxError('In the metadata, %s is not equal to %s.' % (metadata, structure))
 
 
 def checkContents(fileHash, data, metadata):
@@ -120,15 +162,15 @@ def checkContents(fileHash, data, metadata):
     workflows = (u'offline', u'hlt', u'express', u'prompt', u'pcl')
 
     structure = {
-        u'destinationDatabase': (False, unicode),
-        u'inputTag': (False, unicode),
-        u'since': (False, set([int, type(None)])),
-        u'emails': (True, [unicode]),
-        u'userText': (False, unicode),
-        u'destinationTags': (False, {
+        u'destinationDatabase': (True, unicode),
+        u'inputTag': (True, unicode),
+        u'since': (True, (int, type(None))),
+        u'emails': (False, [unicode]),
+        u'userText': (True, unicode),
+        u'destinationTags': (True, {
             unicode: {
-                u'synchronizeTo': (False, workflows),
-                u'dependencies': (True, {
+                u'synchronizeTo': (True, workflows),
+                u'dependencies': (False, {
                     unicode: workflows,
                 }),
             },
