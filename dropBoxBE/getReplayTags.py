69a785324341b5bf296c936e1cd2865f5b644a15
Miguel Ojeda <mojedasa@cern.ch>
2012-10-24 23:00:59 +0200

    Add the getReplayTags.py script and its current output.
    This is an intermediate step to get a sqlite database filled
    with a copy from the snapshot of the tags that are dumped by
    this script, which in turn are all the tags present in
    the original dropBox files.
    
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

---
+++ dropBoxBE/getReplayTags.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python2.6
+'''dropBox backend's script to get the replay tags from the original dropBox files.
+'''
+
+__author__ = 'Miguel Ojeda'
+__copyright__ = 'Copyright 2012, CERN CMS'
+__credits__ = ['Giacomo Govi', 'Salvatore Di Guida', 'Miguel Ojeda', 'Andreas Pfeiffer']
+__license__ = 'Unknown'
+__maintainer__ = 'Miguel Ojeda'
+__email__ = 'mojedasa@cern.ch'
+
+
+import os
+import tarfile
+import json
+
+dropBoxReplayFilesFolder = '/afs/cern.ch/work/m/mojedasa/dropBoxReplayFiles'
+
+outputPairs = set([])
+
+for fileName in os.listdir(dropBoxReplayFilesFolder):
+	tarFile = tarfile.open(os.path.join(dropBoxReplayFilesFolder, fileName))
+
+	names = tarFile.getnames()
+	if len(names) != 2:
+		raise Exception('%s: Invalid number of files in tar file.', fileName)
+
+	baseFileName = names[0].rsplit('.', 1)[0]
+	dbFileName = '%s.db' % baseFileName
+	txtFileName = '%s.txt' % baseFileName
+	if set([dbFileName, txtFileName]) != set(names):
+		raise Exception('%s: Invalid file names in tar file.', fileName)
+	
+	destDB = None
+	tag = None
+	metadata = tarFile.extractfile(txtFileName).readlines()
+	for line in metadata:
+		if line.startswith('destDB'):
+			if destDB is not None:
+				raise Exception('%s: destDB twice.', fileName)
+			destDB = line.split('destDB', 1)[1].strip()
+		elif line.startswith('tag'):
+			if tag is not None:
+				raise Exception('%s: tag twice.', fileName)
+			tag = line.split('tag', 1)[1].strip()
+
+	if destDB is None:
+		raise Exception('%s: destDB not found.', fileName)
+	if tag is None:
+		raise Exception('%s: tag not found.', fileName)
+
+	tarFile.close()
+
+	outputPairs.add((destDB, tag))
+
+with open('outputPairs.json', 'w') as f:
+	json.dump(list(outputPairs), f, sort_keys = True, indent = 4)
+
+outputDict = {}
+for (destDB, tag) in outputPairs:
+	outputDict.setdefault(destDB, []).append(tag)
+
+with open('outputDict.json', 'w') as f:
+	json.dump(outputDict, f, sort_keys = True, indent = 4)
+
