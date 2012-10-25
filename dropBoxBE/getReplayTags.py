0451dd61c483bbc7409098f73f46c4e09b834cb4
Miguel Ojeda <mojedasa@cern.ch>
2012-10-25 15:38:49 +0200

    Convert tabs to spaces (sorry! I was working on vim from home yesterday).
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

--- dropBoxBE/getReplayTags.py
+++ dropBoxBE/getReplayTags.py
@@ -19,47 +19,47 @@ dropBoxReplayFilesFolder = '/afs/cern.ch/work/m/mojedasa/dropBoxReplayFiles'
 outputPairs = set([])
 
 for fileName in os.listdir(dropBoxReplayFilesFolder):
-	tarFile = tarfile.open(os.path.join(dropBoxReplayFilesFolder, fileName))
+    tarFile = tarfile.open(os.path.join(dropBoxReplayFilesFolder, fileName))
 
-	names = tarFile.getnames()
-	if len(names) != 2:
-		raise Exception('%s: Invalid number of files in tar file.', fileName)
+    names = tarFile.getnames()
+    if len(names) != 2:
+        raise Exception('%s: Invalid number of files in tar file.', fileName)
 
-	baseFileName = names[0].rsplit('.', 1)[0]
-	dbFileName = '%s.db' % baseFileName
-	txtFileName = '%s.txt' % baseFileName
-	if set([dbFileName, txtFileName]) != set(names):
-		raise Exception('%s: Invalid file names in tar file.', fileName)
-	
-	destDB = None
-	tag = None
-	metadata = tarFile.extractfile(txtFileName).readlines()
-	for line in metadata:
-		if line.startswith('destDB'):
-			if destDB is not None:
-				raise Exception('%s: destDB twice.', fileName)
-			destDB = line.split('destDB', 1)[1].strip()
-		elif line.startswith('tag'):
-			if tag is not None:
-				raise Exception('%s: tag twice.', fileName)
-			tag = line.split('tag', 1)[1].strip()
+    baseFileName = names[0].rsplit('.', 1)[0]
+    dbFileName = '%s.db' % baseFileName
+    txtFileName = '%s.txt' % baseFileName
+    if set([dbFileName, txtFileName]) != set(names):
+        raise Exception('%s: Invalid file names in tar file.', fileName)
+    
+    destDB = None
+    tag = None
+    metadata = tarFile.extractfile(txtFileName).readlines()
+    for line in metadata:
+        if line.startswith('destDB'):
+            if destDB is not None:
+                raise Exception('%s: destDB twice.', fileName)
+            destDB = line.split('destDB', 1)[1].strip()
+        elif line.startswith('tag'):
+            if tag is not None:
+                raise Exception('%s: tag twice.', fileName)
+            tag = line.split('tag', 1)[1].strip()
 
-	if destDB is None:
-		raise Exception('%s: destDB not found.', fileName)
-	if tag is None:
-		raise Exception('%s: tag not found.', fileName)
+    if destDB is None:
+        raise Exception('%s: destDB not found.', fileName)
+    if tag is None:
+        raise Exception('%s: tag not found.', fileName)
 
-	tarFile.close()
+    tarFile.close()
 
-	outputPairs.add((destDB, tag))
+    outputPairs.add((destDB, tag))
 
 with open('outputPairs.json', 'w') as f:
-	json.dump(list(outputPairs), f, sort_keys = True, indent = 4)
+    json.dump(list(outputPairs), f, sort_keys = True, indent = 4)
 
 outputDict = {}
 for (destDB, tag) in outputPairs:
-	outputDict.setdefault(destDB, []).append(tag)
+    outputDict.setdefault(destDB, []).append(tag)
 
 with open('outputDict.json', 'w') as f:
-	json.dump(outputDict, f, sort_keys = True, indent = 4)
+    json.dump(outputDict, f, sort_keys = True, indent = 4)
 
