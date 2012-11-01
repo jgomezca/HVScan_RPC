56f0a8b6bce0dbb3ee44dcbe8759d3ee04b5c776
Kostas Tamosiunas <kostast@cern.ch>
2012-11-01 17:46:54 +0100

    Module for working with ReleasesXML data
    Data currently stored as a service at https://cmstags.cern.ch/tc/ReleasesXML.
    Module allows to list, filter get information about software releases and related hardware architectures
    
    Signed-off-by: Kostas Tamosiunas <kostast@cern.ch>
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

---
+++ common/ReleasesXML/src/releases.py
@@ -0,0 +1,97 @@
+from collections import namedtuple, defaultdict
+import xml.etree.ElementTree as ET
+
+Release = namedtuple('Release', ['name', 'arch', 'type', 'state'])
+
+class ReleasesXml(object):
+    """
+    Parses data and provides API for ReleasesXML file. During writing documentation file is stored at
+    https://cmstags.cern.ch/tc/ReleasesXML . Assumed that one software release will have one and only one architecture.
+    """
+
+    def __init__(self, data):
+        """
+        XML data to parse. Data parsing execuded during object construction.
+
+        :param data: ReleasesXML file content provided as string
+        """
+        self._data = data
+        self._root = ET.fromstring(self._data)
+        self._parse_data()
+
+    def _parse_data(self):
+        """
+        Data parsing and internal structures preparation.
+        _releases contains data structure {'release_name':ReleseObj, ...}
+        _architectures contains data structure {'architecture_nameA': [ReleseObj1, ReleseObj2...],
+                                                'architecture_nameB':...}
+
+        """
+        self._architectures = defaultdict(list)
+        self._releases = {}
+        arch_elements = self._root.findall("architecture[@name]")
+        for arch_element in arch_elements:
+            architecture_name = arch_element.attrib.get("name")
+            release_elements = arch_element.findall("project[@label]")
+            for release_element in release_elements:
+                release_name = release_element.attrib.get("label")
+                release_type = release_element.attrib.get("type")
+                release_state = release_element.attrib.get("state")
+                release = Release(name=release_name, arch=architecture_name, type=release_type, state=release_state)
+                self._architectures[architecture_name].append(release)
+                self._releases[release_name] = release
+
+
+    def architectures(self):
+        """
+        Returns list of architectures
+
+        :return: list of strings
+        """
+
+        return self._architectures.keys()
+
+    def has_architecture(self, arch_name):
+        """
+        Returns boolean if achtecture exist or not.
+
+        :param arch_name: name of hardware architecture
+        :return: bool
+        """
+
+        return self._architectures.has_key(arch_name)
+
+    def releases(self, arch_name=None):
+        """
+        Returns list of Release objects filtered by architecture name.
+        If arch_name not specified specified - returned all Releases.
+        If architecture name does not exist - returned empty list.
+
+        :param arch_name: name of hardware architecture
+        :return: list of relese objects
+        """
+
+        if arch_name: #architectures filtered by architecture
+            releases = self._architectures[arch_name]
+        else:
+            releases = self._releases.values()
+        return releases
+
+    def has_release(self, release_name):
+        """
+        Returns if release with specified name exits
+
+        :param release_name:
+        :return: bool
+        """
+        return self._releases.has_key(release_name)
+
+    def get_release(self, release_name):
+        """
+        Raises KeyError if release not found
+
+        :param release_name:
+        :return: Returns release object
+        """
+        return self._releases[release_name]
+
---
+++ common/ReleasesXML/test/releases_test.py
@@ -0,0 +1,56 @@
+import collections
+import os
+import sys
+import unittest
+
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+TEST_DIR = os.path.abspath(os.path.join(CURRENT_DIR, "..", "src"))
+sys.path.append(TEST_DIR)
+
+from releases import ReleasesXml, Release
+
+class TestReleasesXml(unittest.TestCase):
+
+    def setUp(self):
+        #URLReleasesXML = "https://cmstags.cern.ch/tc/ReleasesXML"#r = requests.get(URLReleasesXML, verify=False)#r.text
+        curr_dir = os.path.dirname(os.path.abspath(__file__))
+        test_file = os.path.join(curr_dir, "releases.test.xml")
+        data = open(test_file,"rb").read()
+        self.rx = ReleasesXml(data)
+
+    def test_architectures(self):
+        architectures = self.rx.architectures()
+        self.assertIsInstance(architectures, collections.Iterable, "Architectures has be iterable")
+        for arch in architectures:
+            self.assertIsInstance(arch, basestring, "Architecture has to be string or unicode")
+
+    def test_has_architecture(self):
+        self.assertTrue(self.rx.has_architecture("slc5_amd64_gcc472"))
+
+    def test_releases(self):
+        releases = self.rx.releases()
+        self.assertIsInstance(releases, collections.Iterable, "Releases has to be iterable")
+        for rel in releases:
+            self.assertIsInstance(rel, Release, "Release ahs to be object of Release")
+
+
+        filtered_releases = self.rx.releases("slc5_amd64_gcc472")
+        self.assertEqual(len(filtered_releases), 1)
+        self.assertIsInstance(filtered_releases, collections.Sequence)
+        self.assertIsInstance(filtered_releases[0], Release)
+        r = Release(name="CMSSW_6_0_0_patch1",arch="slc5_amd64_gcc472", type="Production", state="Announced")
+        self.assertEqual(filtered_releases[0], r)
+
+    def test_has_release(self):
+        self.assertTrue(self.rx.has_release("CMSSW_6_0_0_patch1"))
+        self.assertFalse(self.rx.has_release("NotExistingReleaseName"))
+
+    def test_get_release(self):
+        r = Release(name="CMSSW_6_0_0_patch1",arch="slc5_amd64_gcc472", type="Production", state="Announced")
+        self.assertEqual(self.rx.get_release("CMSSW_6_0_0_patch1"), r)
+
+        self.assertRaises(KeyError, self.rx.get_release, "NotExistingReleaseName")
+
+
+if __name__ == '__main__':
+    unittest.main()
