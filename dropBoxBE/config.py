f5a72b7e964bd7cb85e8bab872bdd8945fbc11e3
Miguel Ojeda <mojedasa@cern.ch>
2012-11-12 17:02:30 +0100

    tier0Test should use the tier0 backend and tier0 will be the production one (single), also using the tier0 backend when ready.
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

--- dropBoxBE/config.py
+++ dropBoxBE/config.py
@@ -95,27 +95,16 @@ class offline( BaseConfig ) :
                        'prompt' : 'GR10_P_V5'} #-ap: get this from gtList (if we can)
 
 
-class tier0Online( online ) :
+class tier0( online ) :
     def __init__(self) :
-        super( tier0Online, self ).__init__( )
+        super( tier0, self ).__init__( )
 
-        self.backend = 'tier0Online'
-        self.detector = 'T0Online' # was PopCon
+        self.backend = 'tier0'
+        self.detector = 'T0' # was PopCon
 
         self.delay = None     # flag that we are Tier-0 and need to sleep until the next 10-min boundary
 
         # take all the other params from the online base class ...
-
-class tier0Offline( offline ) :
-    def __init__(self) :
-        super( tier0Offline, self ).__init__( )
-
-        self.backend = 'tier0Offline'
-        self.detector = 'T0Offline'
-
-        self.delay = None     # flag that we are Tier-0 and need to sleep until the next 10-min boundary
-
-        # take all the other params from the offline base class ...
 
 
 class test( BaseConfig ) :
@@ -159,7 +148,7 @@ class tier0Test( test ) :
     def __init__(self) :
         super( tier0Test, self ).__init__( )
 
-        self.backend = 'tier0Test'
+        self.backend = 'tier0'
         self.detector = 'T0Test'
 
         self.delay = None     # flag that we are Tier-0 and need to sleep until the next 10-min boundary
