4f1c20a542eb0418476b6533bc1f93024ad285f2
Miguel Ojeda <mojedasa@cern.ch>
2012-11-14 17:16:26 +0100

    Document usage and some pitfalls of the database.py code.
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

--- common/database.py
+++ common/database.py
@@ -77,6 +77,48 @@ class BLOB(str):
 
 
 def transaction(f):
+    '''Decorator for functions with signature f(connection, cursor, ...).
+
+    The decorator will take care of creating a cursor for the transaction
+    and destroying it as needed. The decorator also takes care of connection
+    issues that require a reconnection and retries the *full* transaction again
+    with a new cursor after reconnecting (i.e. it will call f() again).
+    It also rolls-back in case of other exceptions (Oracle-related exceptions
+    that do not require a reconnection, like a failed constraint check
+    or user exceptions).
+
+    Since this decorator retries the full transaction again, you need to
+    be careful when using it for cases where you need to execute actions
+    on external services that may be unreliable (e.g. sending an email)
+    since, if not done properly, you may repeat the action several times
+    on the external service.
+
+    The returned function by the decorator must be called with a Connection
+    object as the first argument, e.g. df(connection, ...). This means
+    this decorator can be used for methods in inherited classes
+    from Connections, like:
+
+        class MyConnection(database.Connection):
+
+            @database.transaction
+            def f(self, cursor):
+                ...
+
+        ...
+
+        myConnection.f()
+
+    or in stand-alone functions like:
+
+        @database.transaction
+        def f(connection, cursor):
+            ...
+
+        ...
+
+        f(connection)
+    '''
+
     def newf(self, *args, **kwargs):
         tries = 2
         while True:
@@ -120,6 +162,9 @@ def transaction(f):
 class Connection(object):
 
     def __init__(self, connectionDictionary):
+        '''Connects automatically to the database.
+        '''
+
         self.connectionDictionary = connectionDictionary
         self.reconnect()
 
@@ -129,16 +174,31 @@ class Connection(object):
 
 
     def reconnect(self):
+        '''Note that this invalidates all cursors as well.
+        '''
+
         logging.info('%s: Connecting to database...', self)
         self.connection = cx_Oracle.connect(service.getCxOracleConnectionString(self.connectionDictionary), threaded = True)
 
 
     def close(self):
+        '''Normally only used for debugging (i.e. testing closing connections
+        in-between a running transaction which can simulate network problems
+        and session timeouts).
+        '''
+
         logging.info('%s: Closing connection...', self)
         self.connection.close()
 
 
     def execute(self, cursor, query, parameters = ()):
+        '''Executes a query taking care of BLOBs in the parameters
+        (which need to be wrapped with the BLOB() class).
+
+        It also logs the query and parameters if in DEBUG level (but avoiding
+        the load of doing so if not).
+        '''
+
         if logging.getLogger().isEnabledFor(logging.DEBUG):
             printParameters = []
             for parameter in parameters:
@@ -146,7 +206,7 @@ class Connection(object):
                     printParameters.append('BLOB %s [len]' % len(parameter))
                 else:
                     printParameters.append(parameter)
-            logging.debug('%s: Database Query: %s Params: %s', self, query, printParameters)
+            logging.debug('%s: Executing: %s Params: %s', self, query, printParameters)
 
         inputSizes = []
         for parameter in parameters:
@@ -164,11 +224,23 @@ class Connection(object):
     # These are not transactions, intended to be used within
     # complex user transactions
     def _commit(self):
+        '''Commits in the database.
+        '''
+
         logging.debug('%s: Committing...', self)
         self.connection.commit()
 
 
     def _fetch(self, cursor, query, parameters = ()):
+        '''Fetches the results from a query, automatically taking care of
+        reading all returned BLOBs into memory.
+
+        Therefore, it has to loop through fetchone() and it is not a good
+        choice if the BLOBs are really big (e.g. if they hold extremely
+        large files that need to be handled one by one and even chunk by chunk
+        since they do not fit in memory).
+        '''
+
         self.execute(cursor, query, parameters)
 
         result = []
@@ -187,11 +259,17 @@ class Connection(object):
     # Trivial transactions
     @transaction
     def fetch(self, cursor, query, parameters = ()):
+        '''Trivial transaction that fetches the results from a single query.
+        '''
+
         return self._fetch(cursor, query, parameters)
 
 
     @transaction
     def commit(self, cursor, query = None, parameters = ()):
+        '''Trivial transaction that commits a single query.
+        '''
+
         if query is not None:
             self.execute(cursor, query, parameters)
 
