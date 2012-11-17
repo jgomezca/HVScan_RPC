64950ccdbfb0e3105950125fe3ba9f6c718109ae
Salvatore Di Guida <salvatore.di.guida@cern.ch>
2012-11-17 22:45:28 +0100

    Allow dropboxBE to run online on top of CMSSW releases installed in a local disk mounted on /data. BEWARE: the script contains the DNS domain name of the CMS technical network: for security reasons, do not distribute worldwide.
    Signed-off-by: Salvatore Di Guida <salvatore.di.guida@cern.ch>
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

--- dropBox/pre.sh
+++ dropBox/pre.sh
@@ -1,9 +1,15 @@
 export SCRAM_ARCH=slc5_amd64_gcc462
-source /afs/cern.ch/cms/cmsset_default.sh
-CMSSW_RELEASE=CMSSW_5_3_4
-CMSSW_REL=/afs/cern.ch/cms/$SCRAM_ARCH/cms/cmssw/$CMSSW_RELEASE/
-
+CMSSW_RELEASE=CMSSW_5_3_6
+DOMAINNAME=`dnsdomainname`
+if [[ ${DOMAINNAME} == "cern.ch" ]] 
+then
+    source /afs/cern.ch/cms/cmsset_default.sh
+    CMSSW_REL=/afs/cern.ch/cms/$SCRAM_ARCH/cms/cmssw/$CMSSW_RELEASE/
+elif [[ ${DOMAINNAME} == "cms" ]]
+then
+    source /data/cmssw/cmsset_default.sh
+    CMSSW_REL=/data/cmssw/$SCRAM_ARCH/cms/cmssw/$CMSSW_RELEASE/
+fi
 pushd $CMSSW_REL >/dev/null
 eval `scramv1 runtime -sh`
 popd >/dev/null
-
