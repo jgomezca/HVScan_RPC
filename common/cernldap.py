4832d48279c80f66aca76e3ddb64a0dad12df94a
Miguel Ojeda <mojedasa@cern.ch>
2012-11-28 19:09:42 +0100

    Add new common/cernldap.py.
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

---
+++ common/cernldap.py
@@ -0,0 +1,48 @@
+'''Common code for searching in the CERN LDAP server.
+'''
+
+__author__ = 'Miguel Ojeda'
+__copyright__ = 'Copyright 2012, CERN CMS'
+__credits__ = ['Giacomo Govi', 'Salvatore Di Guida', 'Miguel Ojeda', 'Andreas Pfeiffer']
+__license__ = 'Unknown'
+__maintainer__ = 'Miguel Ojeda'
+__email__ = 'mojedasa@cern.ch'
+
+
+import logging
+import ldap
+
+
+# TODO: It seems the LDAP connection times out, re-connect automatically if so.
+
+class CERNLDAPError(Exception):
+    '''A common CERNLDAP exception.
+    '''
+
+
+class CERNLDAP(object):
+    '''Class used for searching in the CERN LDAP server.
+    '''
+
+    def __init__(self, server = 'ldap://xldap.cern.ch'):
+        self.server = server
+        self.ldap = ldap.initialize(self.server)
+
+
+    def __str__(self):
+        return 'CERNLDAP %s' % self.server
+
+
+    def getUserInfo(self, username, attributes = None, timeout = None):
+        if timeout is None:
+            timeout = -1
+
+        result = self.ldap.search_st('ou=users,ou=organic units,dc=cern,dc=ch', ldap.SCOPE_SUBTREE, '(cn=%s)' % username, attributes, timeout = timeout)
+        if len(result) == 0:
+            raise CERNLDAPError('Username not found.')
+
+        return result[0][1]
+
+    def getUserEmail(self, username, timeout = None):
+        return self.getUserInfo(username, ['mail'], timeout)['mail'][0]
+
