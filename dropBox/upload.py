91046a798feed9f2db0aacbb86d5248c586f1b6d
Miguel Ojeda <mojedasa@cern.ch>
2012-12-18 12:11:12 +0100

    Increase the version for the upload script.
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

--- dropBox/upload.py
+++ dropBox/upload.py
@@ -8,7 +8,7 @@ __credits__ = ['Giacomo Govi', 'Salvatore Di Guida', 'Miguel Ojeda', 'Andreas Pf
 __license__ = 'Unknown'
 __maintainer__ = 'Miguel Ojeda'
 __email__ = 'mojedasa@cern.ch'
-__version__ = 4
+__version__ = 5
 
 
 import os
