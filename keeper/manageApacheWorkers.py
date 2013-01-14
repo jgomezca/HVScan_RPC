9768950aafc7287cf0d5abf01230ecb8e38babaa
Miguel Ojeda <mojedasa@cern.ch>
2013-01-14 16:30:14 +0100

    Rewritten manageApacheWorkers.py: Simplified and optimized.
    Now a separate class BalancerManager takes care of the interface
    to Apache's balancer-manager. A refresh() method parses the main
    page to get all available balancers, their workers and their status.
    
    URLs are not built anymore (although we still rely on some structure
    in the HTML for the regexps...). This avoids the "http" FIXME
    of a previous commit for the gtc service and also removes
    the dependency on config.py, since we do not need the ports either.
    
    In addition, now all regexps are called once for all the text.
    Overall, the script is simpler and should be easier to update
    for future versions of balancer-module.
    
    manageBackend() now just needs to loop throught the gathered
    information exposed by BalancerManager and trigger any action
    as required.
    
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

--- keeper/manageApacheWorkers.py
+++ keeper/manageApacheWorkers.py
@@ -7,12 +7,12 @@ To debug in case the URLs/parameters change:
      the balancer-manage from the machine's IP itself).
 
   2) Go to the main page for a given virtual host:
-       wget -O - --header 'Host: cms-conddb-prod2.cern.ch' https://vocms151.cern.ch/balancer-manager > ~/balancer-manager.html
+       wget -O - --header 'Host: cms-conddb-prod2.cern.ch' 'https://vocms151.cern.ch/balancer-manager' > ~/balancer-manager.html
 
   3) From there, one can go to the admin page for one of the balancers
      (get the nonce first from the main page):
 
-       wget -O - --header 'Host: cms-conddb-prod2.cern.ch' https://vocms151.cern.ch/balancer-manager?b=admin&w=https://cmsdbbe2.cern.ch:8092/admin&nonce=645e0649-4546-41e1-a405-93e5369b6e02 > ~/balancer-manager-admin.html
+       wget -O - --header 'Host: cms-conddb-prod2.cern.ch' 'https://vocms151.cern.ch/balancer-manager?b=admin&w=https://cmsdbbe2.cern.ch:8092/admin&nonce=645e0649-4556-41e1-a405-a3e5369b6e02' > ~/balancer-manager-admin.html
 
   4) In the bottom, there is the form that needs to be read to know which
      parameters/value to use. e.g. last time it changed from "status_D" with
@@ -43,16 +43,17 @@ import logging
 import optparse
 import re
 
-import config
+
+defaultVirtualHost = 'cms-conddb-prod'
+# Use the current hostname since the server may not listen on 127.0.0.1
+defaultBalancerManagerUrl = 'https://%s/balancer-manager' % socket.gethostname()
 
 
-# Open connection to the virtual host in localhost,
-# using the current hostname (the server may not listen on 127.0.0.1)
-balancerManagerUrl = 'https://%s/balancer-manager' % socket.gethostname()
+def _query(url, virtualHost = None):
+    '''Queries a URL, optionally talking to a specific virtualHost.
 
-
-def _openUrl(url, virtualHost = None):
-    '''Opens a URL, optionally talking to a specific virtualHost.
+    It must set both the SNI and Host header to the virtual host, otherwise
+    Apache might complain.
     '''
 
     if virtualHost is not None:
@@ -60,130 +61,149 @@ def _openUrl(url, virtualHost = None):
             'Host': virtualHost,
         })
 
-    return urllib2.urlopen(url)
+    return urllib2.urlopen(url).read()
 
 
-def findNonce(balancerManagerUrl, virtualHost = None):
-    '''Finds the nonce.
+class BalancerManager(object):
+    '''Class used for managing an Apache's balancer-manager.
     '''
 
-    page = _openUrl(balancerManagerUrl, virtualHost).read()
+    def __init__(self, url, virtualHost = None):
+        self.url = url
+        self.virtualHost = virtualHost
 
-    match = re.search('&nonce=(.*?)"', page)
-    if not match:
-        raise Exception('The nonce was not found.')
-
-    return match.groups()[0]
+        self.refresh()
 
 
-def manageWorker(balancerManagerUrl, balancer, workerUrl, nonce, enable, virtualHost = None):
-    '''Enables or disables a worker.
+    def refresh(self):
+        '''Parses the balancer-manager page to find out the available balancers,
+        the workers of each balancer and its status.
+        '''
+
+        page = _query(self.url, self.virtualHost)
+
+        # Find all balancers
+        self.balancers = re.findall('<h3>LoadBalancer Status for balancer://(.*?)</h3', page)
+        logging.debug('Balancers found: %s', self.balancers)
+
+        # Find all workers for each balancer and their status
+        self.workers = {}
+        for balancer in self.balancers:
+            self.workers[balancer] = {}
+            for (url, nonce) in re.findall('<td><a href="/balancer-manager\?b=%s&w=(.*?)&nonce=(.*?)">' % balancer, page):
+                self.workers[balancer][url] = {
+                    'nonce': nonce,
+                }
+
+            logging.debug('[%s] Workers found: %s', balancer, sorted(self.workers[balancer]))
+
+            for worker in self.workers[balancer]:
+                matches = re.findall('<td><a href="/balancer-manager\?b=%s&w=%s&nonce=%s">%s</a></td><td>(.*?)</td><td>(.*?)</td><td>(.*?)</td><td>(.*?)</td><td>(.*?)</td><td>(.*?)</td><td>(.*?)</td><td>(.*?)</td></tr>' % (balancer, worker, self.workers[balancer][worker]['nonce'], worker), page)
+
+                if len(matches) != 1:
+                    raise Exception('Found more than one status line matching a worker: update the code.')
+
+                # Strip all fields
+                matches = [x.strip() for x in matches[0]]
+
+                # Get the status as a boolean
+                if 'Ok' in matches[4]:
+                    statusBool = True
+                elif 'Dis' in matches[4]:
+                    statusBool = False
+                else:
+                    raise Exception('Unknown status of the worker.')
+
+                self.workers[balancer][worker].update({
+                    'route': matches[0],
+                    'routeRedirection': matches[1],
+                    'loadFactor': matches[2],
+                    'loadSet': matches[3],
+                    'status': matches[4],
+                    'statusBool': statusBool,
+                    'elected': matches[5],
+                    'to': matches[6],
+                    'from': matches[7],
+                })
+
+
+    def enableWorker(self, balancer, worker):
+        '''Enables a worker.
+        '''
+
+        manageWorker(True, balancer, worker)
+
+
+    def disableWorker(self, balancer, worker):
+        '''Disables a worker.
+        '''
+
+        manageWorker(False, balancer, worker)
+
+
+    def manageWorker(self, enable, balancer, worker):
+        '''Enables or disables a worker.
+        '''
+
+        # Do *not* send the other parameters (lf, ls, wr, rr, status_I, ...)
+        # because they would overwrite the current settings.
+        _query('%s?%s' % (self.url, urllib.urlencode({
+            'b': balancer,
+            'w': worker,
+            'nonce': self.workers[balancer][worker]['nonce'],
+            'dw': 'Enable' if enable else 'Disable',
+        })), self.virtualHost)
+
+
+def printStatus(balancerManagerUrl, virtualHost):
+    '''Prints the status of the balancer-manager in this host.
     '''
 
-    if enable:
-        status = 'Enable'
-    else:
-        status = 'Disable'
-
-    # Do *not* send the other parameters (lf, ls, wr, rr, status_I, status_H)
-    # because they would overwrite the current settings.
-    data = {
-        'dw': status,
-        'w': workerUrl,
-        'b': balancer,
-        'nonce': nonce,
-    }
-
-    url = '%s?%s' % (balancerManagerUrl, urllib.urlencode(data))
-    _openUrl(url, virtualHost)
+    balancerManager = BalancerManager(balancerManagerUrl, virtualHost)
+    
+    for balancer in balancerManager.balancers:
+        print '[%s]' % balancer
+        for worker in sorted(balancerManager.workers[balancer]):
+            data = balancerManager.workers[balancer][worker]
+            print '  %s : route = %s, enabled = %s (%s)' % (worker, data['route'], data['statusBool'], data['status'])
 
 
-def getBalancer(service):
-    '''Get the balancer name given a service name.
-    '''
-
-    return service.replace('/', '_').lower()
-
-
-def getStatus(virtualHost):
-    '''Gets the status of all the load balancers for the given virtual host.
-    '''
-
-    lines = _openUrl(balancerManagerUrl, virtualHost).read().splitlines()
-
-    balancers = {}
-    for line in lines:
-        matches = re.findall('<h3>LoadBalancer Status for balancer://(.*?)</h3', line)
-        if len(matches) != 1:
-            continue
-        balancers[matches[0]] = {}
-
-    for balancer in balancers:
-        for line in lines:
-            matches = re.findall('<td><a href="/balancer-manager\?b=%s&w=(.*?)&' % balancer, line)
-            if len(matches) != 1:
-                continue
-            workerUrl = matches[0]
-            matches = re.findall('<td>(.*?)</td>', line)
-            matches = [x.strip() for x in matches]
-            status = matches[5]
-            if 'Ok' in status:
-                statusBool = True
-            elif 'Dis' in status:
-                statusBool = False
-            else:
-                raise Exception('Unknown status of the worker.')
-            balancers[balancer][workerUrl] = {
-                'route': matches[1],
-                'routeRedirection': matches[2],
-                'loadFactor': matches[3],
-                'loadSet': matches[4],
-                'status': matches[5],
-                'statusBool': statusBool,
-                'elected': matches[6],
-                'to': matches[6],
-                'from': matches[7],
-            }
-
-    return balancers
-
-
-def manageBackend(backendHostname, enable, virtualHost):
+def manageBackend(enable, backend, balancerManagerUrl, virtualHost):
     '''Enables or disables all workers (i.e. services) in a backend.
     '''
 
-    if enable:
-        action = 'Enabling'
-    else:
-        action = 'Disabling'
-
     # Remove .cern.ch if provided
-    if backendHostname.endswith('.cern.ch'):
-        backendHostname = backendHostname[:-len('.cern.ch')]
+    if backend.endswith('.cern.ch'):
+        backend = backend[:-len('.cern.ch')]
 
-    logging.info('%s all workers for the %s virtual host in the %s backend...', action, virtualHost, backendHostname)
+    logging.info('%s all workers for the %s backend in the %s virtual host...', 'Enabling' if enable else 'Disabling', backend, virtualHost)
 
-    nonce = findNonce(balancerManagerUrl, virtualHost)
+    balancerManager = BalancerManager(balancerManagerUrl, virtualHost)
 
-    for service in config.servicesConfiguration:
-        listeningPort = config.servicesConfiguration[service]['listeningPort']
+    for balancer in balancerManager.balancers:
+        match = False
 
-        balancer = getBalancer(service)
+        for worker in balancerManager.workers[balancer]:
+            if backend in worker:
+                match = True
 
-        # FIXME: We could get the list of URLs from the main page directly
-        protocol = 'https'
-        if service == 'gtc':
-            protocol = 'http'
+                # Warn if it was already in the requested state
+                if balancerManager.workers[balancer][worker]['statusBool'] == enable:
+                    logging.warn('The state of backend %s for balance %s was already %s', backend, balancer, balancerManager.workers[balancer][worker]['statusBool'])
 
-        workerUrl = '%s://%s.cern.ch:%s/%s' % (protocol, backendHostname, listeningPort, service)
+                balancerManager.manageWorker(enable, balancer, worker)
 
-        manageWorker(balancerManagerUrl, balancer, workerUrl, nonce, enable, virtualHost = virtualHost)
+        if not match:
+            logging.warn('No matching workers for the %s backend in balancer %s. Current ones: %s', backend, balancer, sorted(balancerManager.workers[balancer]))
 
-    status = getStatus(virtualHost)
-    for balancer in status:
-        for backend in status[balancer]:
-            if backendHostname in backend and status[balancer][backend]['statusBool'] != enable:
-                logging.warn('The %s backend\'s state is still %s', backendHostname, status[balancer][backend]['statusBool'])
+    # Check afterwards (this prevents issues if Apache's version changes
+    # which may change the parameters of the balancer-manager forms)
+    balancerManager.refresh()
+
+    for balancer in balancerManager.balancers:
+        for worker in balancerManager.workers[balancer]:
+            if backend in worker and balancerManager.workers[balancer][worker]['statusBool'] != enable:
+                logging.warn('The state of backend %s for balance %s is still %s. Check for misconfiguration or bugs in this script.', backend, balancer, balancerManager.workers[balancer][worker]['statusBool'])
 
 
 def main():
@@ -192,28 +212,33 @@ def main():
 
     parser = optparse.OptionParser(usage =
         'Usage: %prog status\n'
-        '   or: %prog <command> <backendHostname>\n'
+        '   or: %prog <command> <backend>\n'
+        '\n'
         '  where commmand can be status, enable or disable.\n'
+        '\n'
+        '  e.g.: %prog status  cmsdbbe1 -v cms-conddb-prod2\n'
+        '  e.g.: %prog disable cmsdbbe1 -v cms-conddb-prod2\n'
     )
 
     parser.add_option('-v', '--virtualHost',
         dest = 'virtualHost',
-        default = 'cms-conddb-prod',
+        default = defaultVirtualHost,
         help = 'Virtual host to manage. Use this option to test debugging virtualHosts (e.g. cms-conddb-prod2). Default: %default'
+    )
+
+    parser.add_option('-u', '--balancerManagerUrl',
+        dest = 'balancerManagerUrl',
+        default = defaultBalancerManagerUrl,
+        help = 'URL pointing to the balancer-manager. Default: %default'
     )
 
     (options, arguments) = parser.parse_args()
 
     if len(arguments) == 1 and arguments[0] in ['status']:
-        balancers = getStatus(**vars(options))
-        for balancer in balancers:
-            print '[%s]' % balancer
-            for workerUrl in sorted(balancers[balancer]):
-                worker = balancers[balancer][workerUrl]
-                print '    %s %s %s (%s)' % (workerUrl, worker['route'], worker['statusBool'], worker['status'])
-        return
-    elif len(arguments) == 2 and arguments[0] in ['enable', 'disable']:
-        return manageBackend(arguments[1], arguments[0] == 'enable', **vars(options))
+        return printStatus(balancerManagerUrl = options.balancerManagerUrl, virtualHost = options.virtualHost)
+
+    if len(arguments) == 2 and arguments[0] in ['enable', 'disable']:
+        return manageBackend(arguments[0] == 'enable', arguments[1], balancerManagerUrl = options.balancerManagerUrl, virtualHost = options.virtualHost)
 
     parser.print_help()
     return -2
