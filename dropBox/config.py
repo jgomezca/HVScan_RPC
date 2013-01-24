63168af4aca9fac2a3a71cc7cda74e8c76df5018
Miguel Ojeda <mojedasa@cern.ch>
2013-01-24 18:10:43 +0100

    Move the statusCode to the beginning of the subject line.
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

--- dropBox/config.py
+++ dropBox/config.py
@@ -28,8 +28,8 @@ notificationsEgroup = 'cms-cond-dropbox-notifications@cern.ch'
 
 
 # Email template
-subjectTemplate = jinja2.Template('[DropBox] {{fileName}}: {{statusCode}} ({{statusString}})')
-bodyTemplate = jinja2.Template('''[DropBox] {{fileName}}: {{statusCode}} ({{statusString}})
+subjectTemplate = jinja2.Template('[DropBox] {{statusCode}} {{fileName}}: {{statusString}}')
+bodyTemplate = jinja2.Template('''[DropBox] {{statusCode}} {{fileName}}: {{statusString}}
 
          fileName: {{fileName}}
          fileHash: {{fileHash}}
