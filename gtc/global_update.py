64e23a6978c578bc2dff197d9d6ee2de4ac767a5
Miguel Ojeda <mojedasa@cern.ch>
2013-03-20 12:04:20 +0100

    Disable gtc's cronjob in dev and int since the DB will be updated by Dimitar.
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

--- gtc/global_update.py
+++ gtc/global_update.py
@@ -8,6 +8,10 @@ def main():
         logging.info('Another instance is running, exiting...')
         return
 
+    if service.settings['productionLevel'] in ['dev', 'int']:
+        logging.info('Disabled in %s since the DB will be updated by Dimitar.', service.settings['productionLevel'])
+        return
+
     subprocess.call('python src/manage.py global_update', shell=True)
 
 
