aa36fa82d758632da16a7cca41aacfec5ed3826f
Miguel Ojeda <mojedasa@cern.ch>
2013-04-05 15:57:22 +0200

    Improved emulateDropBoxBE.py for multiple uploads.
    This was used on 2013-04-04 and 2013-04-05 to finish
    the uploads (fileLog, downloadLog, globalLog and emails)
    of the daylight saving time issue.
    
    Signed-off-by: Miguel Ojeda <mojedasa@cern.ch>

--- dropBoxBE/emulateDropBoxBE.py
+++ dropBoxBE/emulateDropBoxBE.py
@@ -6,18 +6,78 @@ for three uploads done on 2013-02-06, since updating/uploading status/code
 failed since the CERN SSO session expired due to extremely long exportations
 due to a huge load in the Oracle cms_orcon_prod database.
 
-To use it, carefully modify the parameters below and then run:
+Then it was improved to allow for multiple uploads easily and to avoid having
+to split the web logs into the downloadLog and globalLog, since we needed
+to perform 5 manual updates after the daylight saving time issue.
+
+To use it, carefully craft the required files at runPath and then run:
 
   keeper.py run dropBoxBE emulateDropBoxBE.py
 
-Note: you will need to extract the real logs from the real backend and divide
-them into the fileLog/downloadLog/globalLog as required.
+Note: You will need to uncomment the dropBoxBE in the keeper/config.py.
 
-Note: if the run processed more than one file, run the script once for the
-first one, and then set updateRunLog to False, since the "runLog" is common
-for both and you shouldn't upload it more than once.
+The script asks before doing something, so you can do only a subset of
+the operations as needed and review them beforehand.
 
-Note: you will need to uncomment the dropBoxBE in the keeper/config.py.
+Create a folder which contains:
+
+  * files.json: a list of runs with their properties and files. Looks like [1].
+  * <fileHash>.log: the fileLog per file, you can copy it directly from
+    the backend unchanged, including the filename.
+  * <runCreationTimestamp>.log: the web log per file. See below.
+
+The hard part about the procedure is to get the proper downloadLogs and runLogs.
+Therefore, instead of manually splitting them into downloadLogs and runLogs,
+we just simple use empty downloadLogs with just this line:
+
+    # Manual upload: see the globalLog for the full web log.
+
+And for the globalLogs, we copy the relevant portion of the web log, from:
+
+    INFO: Processing all files...
+
+to:
+
+    INFO: Processing all files done; waiting 30 seconds for the next run.
+
+and we add this line in the top:
+
+    # Manual upload: this is the full web log.
+
+This simplifies things and allows for more automatic uploads,
+without lose of information. Adding these lines is automatic and is done by
+this script. Therefore, you only need to provide the web log in
+the <runCreationTimestamp>.log files.
+
+See an example of the files in the folder in [2] for [1].
+
+Note: The <runCreationTimestamp>.log files contain a space in the name.
+
+[1]
+
+[
+["2013-04-03 10:20:27,821", "online", 9999, -10, 212234, [["a2fd5987b989a9c57e091ec42ff7f9dd0e71aa69", 4999], ["bcc394929086b313fc47dce67d23bc0b604102ff", 4999]]],
+["2013-04-03 10:21:04,146", "online", 9999, -10, 212234, [["e881544be662aee0c288357d0724968c4ecfb35d", 4999]]],
+["2013-04-03 10:21:37,720", "online", 9999, -10, 212234, [["5e94c68b203c610037dc56dc3250a2cce0daa40a", 4999], ["83512bc95265f70c0cc5ad35007223d2b940f816", 4999]]],
+["2013-04-03 17:20:02,276", "online", 9999, -10, 212234, [["6baa28e382c79f20be74caa40b230d6f3548c4ac", 4999]]],
+["2013-04-03 17:28:58,632", "online", 9999, -10, 212234, [["a0143e8075e79c762671d409f63ec3ba6ff48dc1", 4999]]]
+]
+
+[2]
+
+files.json
+2013-04-03 10:20:27,821.log
+2013-04-03 10:21:04,146.log
+2013-04-03 10:21:37,720.log
+2013-04-03 17:20:02,276.log
+2013-04-03 17:28:58,632.log
+5e94c68b203c610037dc56dc3250a2cce0daa40a.log
+6baa28e382c79f20be74caa40b230d6f3548c4ac.log
+83512bc95265f70c0cc5ad35007223d2b940f816.log
+a0143e8075e79c762671d409f63ec3ba6ff48dc1.log
+a2fd5987b989a9c57e091ec42ff7f9dd0e71aa69.log
+bcc394929086b313fc47dce67d23bc0b604102ff.log
+e881544be662aee0c288357d0724968c4ecfb35d.log
 '''
 
 __author__ = 'Miguel Ojeda'
@@ -30,6 +90,8 @@ __email__ = 'mojedasa@cern.ch'
 
 import sys
 import logging
+import os
+import json
 
 import service
 
@@ -37,64 +99,81 @@ import config
 import StatusUpdater
 
 
-fileHash = '725eec71893884224fee4b0c7638f5ceeceabe99'
-creationTimeStamp = '2013-02-06 16:51:02,200'
-backend = 'tier0'
-fileStatus = 4210
-runStatus = 9910
-fileLogPath = 'fileLog.log'
-downloadLogPath = 'downloadLog.log'
-globalLogPath = 'globalLog.log'
-updateRunLog = False
+runsPath = '~/dropBoxEmulate/'
+
+downloadLog = '# Manual upload: see the globalLog for the full web log.\n'
+globalLogPrefix = '# Manual upload: this is the full web log.\n'
 
 
 def main():
     '''Entry point.
     '''
 
-    if backend == 'online':
-        cfg = config.online()
-    elif backend == 'tier0':
-        cfg = config.tier0()
-    elif backend == 'offline':
-        cfg = config.offline()
-    else:
-        raise Exception('Unsupported backend to emulate.')
+    with open(os.path.join(os.path.expanduser(runsPath), 'files.json'), 'rb') as f:
+        runs = json.loads(f.read())
 
-    cfg.proxy = None
+    for (runLogCreationTimestamp, runBackend, runStatus, fcsRun, hltRun, files) in runs:
+        runLogCreationTimestamp = str(runLogCreationTimestamp)
+        runBackend = str(runBackend)
 
-    with open(fileLogPath, 'r') as f:
-        fileLog = f.read()
+        logging.info('Run %s: %s, %s, %s, %s', runLogCreationTimestamp, runBackend, runStatus, fcsRun, hltRun)
 
-    with open(downloadLogPath, 'r') as f:
-        downloadLog = f.read()
 
-    with open(globalLogPath, 'r') as f:
-        globalLog = f.read()
+        # Create the emulated statusUpdater
 
-    logging.info('downloadLog = \n[[[\n%s\n]]]', downloadLog)
-    logging.info('globalLog = \n[[[\n%s\n]]]', globalLog)
-    logging.info('fileLog = \n[[[\n%s\n]]]', fileLog)
-    logging.info('fileHash = %s', fileHash)
-    logging.info('creationTimeStamp = %s', creationTimeStamp)
-    logging.info('backend = %s', backend)
-    logging.info('fileStatus = %s', fileStatus)
-    logging.info('runStatus = %s', runStatus)
-    logging.info('updateRunLog = %s', updateRunLog)
-    logging.info('baseUrl = %s', cfg.baseUrl)
+        if runBackend == 'online':
+            cfg = config.online()
+        elif runBackend == 'tier0':
+            cfg = config.tier0()
+        elif runBackend == 'offline':
+            cfg = config.offline()
+        else:
+            raise Exception('Unsupported backend to emulate.')
 
-    if raw_input('Are you sure? ').lower() != 'y':
-        logging.error('Stopped.')
-        return -1
+        cfg.proxy = None
 
-    statusUpdater = StatusUpdater.StatusUpdater(cfg)
-    statusUpdater.creationTimeStamp = creationTimeStamp
-    statusUpdater.backend = backend
-    statusUpdater.updateFileStatus(fileHash, fileStatus)
-    statusUpdater.uploadFileLog(fileHash, fileLog)
-    if updateRunLog:
-        statusUpdater.uploadRunLog(downloadLog, globalLog)
-        statusUpdater.updateRunStatus(runStatus)
+        statusUpdater = StatusUpdater.StatusUpdater(cfg)
+        statusUpdater.creationTimeStamp = runLogCreationTimestamp
+        statusUpdater.backend = runBackend
+
+
+        # Ask and run actions
+
+        if raw_input('updateRunStatus(%s). Are you sure? ' % runStatus).lower() == 'y':
+            statusUpdater.updateRunStatus(runStatus)
+        else:
+            logging.warning('Skipped.')
+
+        if raw_input('updateRunRunInfo(%s, %s). Are you sure? ' % (fcsRun, hltRun)).lower() == 'y':
+            statusUpdater.updateRunRunInfo(fcsRun, hltRun)
+        else:
+            logging.warning('Skipped.')
+
+        with open(os.path.join(os.path.expanduser(runsPath), '%s.log' % runLogCreationTimestamp), 'rb') as f:
+            globalLog = globalLogPrefix + f.read()
+
+        if raw_input('uploadRunLog(%s, %s). Are you sure? ' % (repr(downloadLog), repr(globalLog))).lower() == 'y':
+            statusUpdater.uploadRunLog(downloadLog, globalLog)
+        else:
+            logging.warning('Skipped.')
+
+        for (fileHash, fileStatus) in files:
+            fileHash = str(fileHash)
+
+            logging.info('    File %s: %s', fileHash, fileStatus)
+
+            with open(os.path.join(os.path.expanduser(runsPath), '%s.log' % fileHash), 'rb') as f:
+                fileLog = f.read()
+
+            if raw_input('statusUpdater.updateFileStatus(%s, %s). Are you sure? ' % (repr(fileHash), fileStatus)).lower() == 'y':
+                statusUpdater.updateFileStatus(fileHash, fileStatus)
+            else:
+                logging.warning('Skipped.')
+
+            if raw_input('statusUpdater.uploadFileLog(%s, %s). Are you sure? ' % (repr(fileHash), repr(fileLog))).lower() == 'y':
+                statusUpdater.uploadFileLog(fileHash, fileLog)
+            else:
+                logging.warning('Skipped.')
 
 
 if __name__ == '__main__':
